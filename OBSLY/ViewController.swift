fd738cf7521807826fd3d856c2305a14a46e2e9d
Tung Fam <tung@pixpie.co>
2016-09-05 11:43:02 +0300

    Initial Commit

---
+++ OBSLY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OBSLY
+//
+//  Created by Tung Fam on 9/5/16.
+//  Copyright © 2016 Tung Fam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
