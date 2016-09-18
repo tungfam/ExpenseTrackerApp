31d18dc0fe3b535daaab05ba518a82eb579e4f07
Tung Fam <tung@pixpie.co>
2016-09-18 15:53:12 +0300

    added core data; saving books in core data; getting the key for the book

---
+++ OBSLY/DataController.swift
@@ -0,0 +1,13 @@
+//
+//  DataController.swift
+//  OBSLY
+//
+//  Created by Tung Fam on 9/17/16.
+//  Copyright © 2016 Tung Fam. All rights reserved.
+//
+
+import UIKit
+
+class DataController: NSObject {
+
+}
